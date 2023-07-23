ba16fb3fd2008531aabda9fa142b0b7f682d4575
Rov <140263579+phoenixedu@users.noreply.github.com>
2023-07-23 13:31:58 +0500

    Update settings.py

--- phoenix_v_0/settings.py
+++ phoenix_v_0/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-_6fu$++@hf&74o8#v^a-0=z1czvb^#b-zzhm*+0n^z)gs+=23j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
